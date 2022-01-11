f13c7ca10499b721e4c8d843aa134ae348c2d2b4
tomagato <tgannot@gmail.com>
2022-01-12 01:37:30 +0200

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -6,71 +6,13 @@ import Population from './components/population/Population';
 import Table from './components/table/Table';
 
 function App() {
-  const [planets, setPlanets] = useState([]);
-  const [people, setPeople] = useState([]);
   const [vehicles, setVehicles] = useState([]);
+  const [tableTuple, setTableTuple] = useState([
+    { vehicleName: '', planet: [{planetName: '', poplulation: '' }], pilotNames: [] },
+  ]);
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(true);
-  const [maxPopulationForVehicle, setMaxPopulationForVehicle] = useState([]);
   const NUM_ITEMS_ARRAY = 10;
-
-  useEffect(async () => {
-    await axios('https://swapi.py4e.com/api/planets/?page=1')
-      .then(async (response) => {
-        var currPage = 1;
-        const pageCount = Math.ceil(
-          Number(response.data.count) / NUM_ITEMS_ARRAY
-        );
-        while (pageCount != currPage - 1) {
-          let currPageStr = currPage.toString();
-          await axios(`https://swapi.py4e.com/api/planets/?page=${currPageStr}`)
-            .then((response) => {
-              setPlanets((planets) => [...planets, ...response.data.results]);
-            })
-            .catch((error) => {
-              console.error('Error fetching data inside 2nd axios: ', error);
-              setError(error);
-            });
-          currPage++;
-        }
-      })
-      .catch((error) => {
-        console.error('Error fetching data: ', error);
-        setError(error);
-      })
-      .finally(() => {
-        setLoading(false);
-      });
-  }, []);
-
-  useEffect(async () => {
-    await axios('https://swapi.py4e.com/api/people/?page=1')
-      .then(async (response) => {
-        var currPage = 1;
-        const pageCount = Math.ceil(
-          Number(response.data.count) / NUM_ITEMS_ARRAY
-        );
-        while (pageCount != currPage - 1) {
-          let currPageStr = currPage.toString();
-          await axios(`https://swapi.py4e.com/api/people/?page=${currPageStr}`)
-            .then((response) => {
-              setPeople((people) => [...people, ...response.data.results]);
-            })
-            .catch((error) => {
-              console.error('Error fetching data inside 2nd axios: ', error);
-              setError(error);
-            });
-          currPage++;
-        }
-      })
-      .catch((error) => {
-        console.error('Error fetching data: ', error);
-        setError(error);
-      })
-      .finally(() => {
-        setLoading(false);
-      });
-  }, []);
 
   useEffect(async () => {
     await axios('https://swapi.py4e.com/api/vehicles/?page=1')
@@ -106,15 +48,30 @@ function App() {
       });
   }, []);
 
-  const findMaxVehicles = () => {
-    return (
-      <li>
-        {vehicles.map((vehicle) => vehicle.pilots)}
-        {people.map((people) => people.name)}
-        {planets.map((planet) => planet.name)}
-      </li>
-    );
+  const getPilots = async (pilotUrl,vehicle) => {
+    await axios(pilotUrl)
+      .then(async (response) => {
+        setTableTuple((tableTuple) => [
+          ...tableTuple,
+          [{ vehicleName: vehicle, planet: [{planetName: '', poplulation: '' }], pilotNames: [response.data.name] }],
+        ], console.log(response.data.name));
+      })
+      .catch((error) => {
+        console.error('Error fetching data: ', error);
+        setError(error);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
   };
+
+  const findMaxVehicles = () => (
+    <li>
+      {vehicles.map((vehicle) =>
+        vehicle.pilots.map((pilotUrl) => getPilots(pilotUrl,vehicle.name))
+      )}
+    </li>
+  );
 
   return (
     <div className='App'>
