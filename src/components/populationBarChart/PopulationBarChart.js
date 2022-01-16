f994d14a91e636ceaee3301c3430954dfbd751cf
Tomagato <81528869+Tomagato@users.noreply.github.com>
2022-01-16 03:22:52 +0200

    removed cl

--- src/components/populationBarChart/PopulationBarChart.js
+++ src/components/populationBarChart/PopulationBarChart.js
@@ -3,14 +3,7 @@ import React, { useRef, useEffect } from 'react'
 const Canvas = props => {
   
   const canvasRef = useRef(null)
-  
-  const draw = (ctx, frameCount) => {
-    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height)
-    ctx.fillStyle = '#000000'
-    ctx.beginPath()
-    ctx.arc(50, 100, 20*Math.sin(frameCount*0.05)**2, 0, 2*Math.PI)
-    ctx.fill()
-  }
+ 
 
   const drawBar = (ctx,canvas) => {
     ctx.fillStyle = 'navy';
@@ -34,9 +27,7 @@ const Canvas = props => {
     
     const canvas = canvasRef.current
     const context = canvas.getContext('2d')
-    let frameCount = 0
-    let animationFrameId
-    
+
     //Our draw came here
     drawBar(context,canvas)
     
